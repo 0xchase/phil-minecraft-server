0f5002b52859e4d96381a7b8e7bf74eabde71755
chase1635321 <chasekanipe@gmail.com>
2020-03-18 11:14:13 -0400

    Backup Wed Mar 18 11:14:13 EDT 2020

--- backup.sh
+++ backup.sh
@@ -2,11 +2,12 @@ ssh -T git@github.com
 clear
 
 while true; do
+	echo ________________________________________________________________________________________
 	clear
 	git add .;
 	git commit -m "Backup $(date)";
 	git push;
 	echo "";
-	sleep 10;
+	sleep 1800;
 done
 
